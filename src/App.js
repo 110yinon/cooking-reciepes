beebf815889235f1a239df2ac1221c50101b71ac
goo <goo>
2022-05-26 17:15:14 +0300

    recipes list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Bar from './components/Bar';
+import Recipe from './components/Recipe';
+import { useFetch } from './hooks/useFetch';
 
 function App() {
+  const [url, setUrl] = useState('http://localhost:3000/recipes');
+  const { data: recipes, isPending, error } = useFetch(url);
+
+  console.log(recipes);
+
   return (
     <div className="App">
-      <Bar />      
+      <Bar />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading recipes...</div>}
+      <div className='recipes-list container'>
+        {recipes && recipes.map(recipe => <Recipe recipe={recipe} key={recipe.id} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,12 @@
+import './recipe.css'
+export default function Recipe({ recipe }) {
+    console.log('recipe:', recipe);
+    return (
+        <div className='recipe'>
+            <h2 className='title'>{recipe.title}</h2>
+            <h4 className='cooking-time'>{recipe.cookingTime}</h4>
+            <p className='method' >{recipe.method}</p>
+            <button>Cook This</button>
+        </div>
+    );
+}
