7de6cf3d17beb58c3cc4d5337158920090e048aa
goo <goo>
2022-06-07 23:27:36 +0300

    bar styles

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -4,16 +4,18 @@ import './bar.css';
 function Bar({ changeHandler }) {
 
     return (
-        <div className="bar container">
-            <h1><Link to="/">Cooking Ninja</Link></h1>
-            <div className='ctrl-bar'>
-                <label>
-                    <span>Search: </span>
-                    <input type="text" onChange={changeHandler} />
-                </label>
-                <button><Link to="/recipe">Create Recipe</Link></button>
+        <header>
+            <div className="bar">
+                <h1><Link to="/">Cooking Ninja</Link></h1>
+                <div className='ctrl-bar'>
+                    <label>
+                        <span>Search: </span>
+                        <input type="text" onChange={changeHandler} />
+                    </label>
+                    <button><Link to="/recipe">Create Recipe</Link></button>
+                </div>
             </div>
-        </div>
+        </header>
     );
 }
 
